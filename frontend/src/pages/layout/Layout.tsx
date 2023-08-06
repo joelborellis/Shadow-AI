4be30f5646a87bfc761338f10e826b24c4833a41
Joel Borellis <joelborellis@outlook.com>
2023-08-06 10:12:35 -0500

    added the chat history thats almost finished.  removed the login checks in Chat

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -1,28 +1,15 @@
 import { Outlet, Link } from "react-router-dom";
 import styles from "./Layout.module.css";
-import { Settings16Filled } from "@fluentui/react-icons";
 
-import { useEffect, useState } from "react";
 import { useIsAuthenticated, useMsal } from "@azure/msal-react";
 import { SignInButton } from "../../components/Signin/SignInButton";
 import { SignOutButton } from "../../components/Signin/SignOutButton";
 import { GraphContent } from "../../components/Signin/GraphContent";
-import { selectHistoryRequest, ChatMessage } from "../../api";
-import { loginRequest } from "../../authConfig";
 import { Stack } from "@fluentui/react";
 
 const Layout = (props: any) => {
-  const [isSharePanelOpen, setIsSharePanelOpen] = useState<boolean>(false);
-  const [copyClicked, setCopyClicked] = useState<boolean>(false);
-  const [copyText, setCopyText] = useState<string>("Copy URL");
   const isAuthenticated = useIsAuthenticated();
-  const { instance, accounts } = useMsal();
 
-  useEffect(() => {
-    if (copyClicked) {
-      setCopyText("Copied URL");
-    }
-  }, [copyClicked]);
 
   return (
     <div className={styles.layout}>
